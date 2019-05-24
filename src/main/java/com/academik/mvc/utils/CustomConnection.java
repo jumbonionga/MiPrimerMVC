9a007cfa14f63bff48617b1d7fd5fb9e4deb295a
jumbonionga <jumbonionga+github@gmail.com>
2019-05-23 18:54:43 -0600

    edición de CustomConnection para que se pueda utilizar

--- src/main/java/com/academik/mvc/utils/CustomConnection.java
+++ src/main/java/com/academik/mvc/utils/CustomConnection.java
@@ -23,11 +23,11 @@ public class CustomConnection {
     
     private static final String DRIVER = "org.postgresql.Driver";
     private static final String HOST = "localhost";
-    private static final String DATA_BASE = "academik";
+    private static final String DATA_BASE = "Education";
     private static final int PORT = 5432;
     private static final String URL = "jdbc:postgresql://" + HOST + ':' + PORT + '/' + DATA_BASE;
     private static final String USER = "postgres";
-    private static final String PASS = "1234qwer";
+    private static final String PASS = "admin";
     
     public Connection getConnection()
             throws ClassNotFoundException, SQLException {
